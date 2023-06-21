fdb84de9e5f2d90c48f7d5d6b5a4d4af800f6128
WallisonRP <wallison0076@gmail.com>
2023-06-21 20:44:53 -0300

    teste json

--- api/product.js
+++ api/product.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
 const router = express.Router();
+const filePath = 'data.json';
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
 
 /**
  * GET product list
@@ -9,10 +17,15 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
     try{
-        res.json({
-            status: 200,
-            message: "Get data has successfully",
-        })
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+              console.error(err);
+              res.status(500).send('Erro interno do servidor');
+            } else {
+              const json = JSON.parse(data);
+              res.json(json);
+            }
+          });
     } catch (error) {
         console.error(error);
         return res.status(500).send("server error");
